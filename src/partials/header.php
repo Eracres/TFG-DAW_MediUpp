7f1c1635be509b35cd35b4a6ed36473ceaed57b4
Eracres <122403544+Eracres@users.noreply.github.com>
2024-11-19 09:38:17 +0100

    Create header.php

---
+++ src/partials/header.php
@@ -0,0 +1,17 @@
+<!-- header.php -->
+
+<header class="app-header">
+    <!-- Logos -->
+    <div class="app-header-logo">
+        <img src="../../resources/logo/Logo(final).png" alt="Logo" class="app-header-logo-img">
+    </div>
+    <div class="app-header-title">
+        <img src="../../resources/logo/Titulo(final).png" alt="Título" class="app-header-title-img">
+    </div>
+    <!-- Botón de Modo Oscuro/Claro -->
+    <div class="toggle-switch-container">
+        <i class="fas fa-sun text-yellow-500"></i> <!-- Icono del sol -->
+        <button class="toggle-switch dark-mode" aria-label="Toggle dark mode"></button>
+        <i class="fas fa-moon text-gray-500"></i> <!-- Icono de la luna -->
+    </div>
+</header>
