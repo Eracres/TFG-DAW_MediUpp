061b0e5637dde0040e05c86e72732997cf86b80b
Eracres <122403544+Eracres@users.noreply.github.com>
2024-11-21 17:46:10 +0100

    Update header.php

--- src/partials/header.php
+++ src/partials/header.php
@@ -8,10 +8,15 @@
     <div class="app-header-title">
         <img src="../../resources/logo/Titulo(final).png" alt="Título" class="app-header-title-img">
     </div>
-    <!-- Botón de Modo Oscuro/Claro -->
+    <!-- Botón de Modo Oscuro/Claro y Notificaciones -->
     <div class="toggle-switch-container">
+        <!-- Botón de Notificaciones -->
+        <button class="notification-button app-header-modo" aria-label="Notifications">
+            <i class="fa-solid fa-inbox"></i>
+        </button>
         <i class="fas fa-sun text-yellow-500"></i> <!-- Icono del sol -->
         <button class="toggle-switch dark-mode" aria-label="Toggle dark mode"></button>
         <i class="fas fa-moon text-gray-500"></i> <!-- Icono de la luna -->
+       
     </div>
 </header>
