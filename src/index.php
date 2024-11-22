221bb04a9e3a5ec487ed4a12a503c8be73a3afa3
Eracres <122403544+Eracres@users.noreply.github.com>
2024-11-22 14:25:36 +0100

    Update index.php

---
+++ src/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MediUpp | Bienvenido</title>
+    <link href="css/output.css" rel="stylesheet">
+    <link rel="icon" href="../../../resources/logo/Logo (final).png" type="image/x-icon">
+    <script defer src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+<body class="index-body">
+    <div class="index-container">
+        <div class="index-logo-wrapper">
+            <img src="../resources/logo/MediUpp(final).png" alt="MediUpp Logo" class="index-logo">
+        </div>
+        <div class="index-title-container">
+            <h1 class="index-title">Entra, Diviértete, Comparte</h1>
+        </div>
+        <p class="index-description">
+            La plataforma ideal para gestionar y compartir tus eventos multimedia. Únete a nosotros y explora las posibilidades.
+        </p>
+        <div class="flex justify-center">
+            <a href="pages\auth\register.php" class="index-button index-button-register">
+                Registrarse
+            </a>
+            <a href="login.php" class="index-button index-button-login">
+                Acceder
+            </a>
+        </div>
+    </div>
+</body>
+</html>
